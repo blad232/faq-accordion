756047859d13b86426c769c7567736a4c25a7217
Bladimir Castro <blad232@gmail.com>
2024-03-06 13:06:17 -0400

    Second Commit

--- SRC/index.js
+++ SRC/index.js
@@ -1,14 +1,25 @@
-const card = document.getElementById("card");
-card.addEventListener("click", (evento) => {
-	console.log("Diste click en: " + evento.target.innerText);
-});
+'use strict';
+//Selecting elements by classes.
+const card__img = document.getElementsByClassName("card__img");
+const paragragh = document.getElementsByClassName("card__block-details");
+const card__headingBlock = document.getElementsByClassName("card__heading-block");
 
-minus.addEventListener("click", () => {
-	plus.classList.toggle("card__block-plus");
-});
-minus[2].addEventListener("click", () => {
-	console.log(minus[2]);
-});
-minus[3].addEventListener("click", () => {
-	console.log(minus[3]);
-});
+
+//adding events 
+for (let i = 0; i < card__img.length; i++) {
+	card__img[i].addEventListener("click", () => {
+		paragragh[i].classList.toggle("--active");
+		card__headingBlock[i].classList.toggle("--border");
+
+		if (paragragh[i].classList == "card__block-details --active") {
+			card__img[i].src = "./assets/images/icon-minus.svg";
+			card__headingBlock[3].classList.add("--border");
+		} else {
+			card__img[i].src = "./assets/images/icon-plus.svg";
+			card__headingBlock[3].classList.add("--border");
+		}
+
+
+	});
+}
+
